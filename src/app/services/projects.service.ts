82dc7cd42fe201c22437de0a58f929f6467614b6
lappranti <ndeurys@gmail.com>
2023-08-21 18:03:02 +0000

    first commit

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ProjectsService {
   private projectList: any = [
@@ -10,88 +10,93 @@ export class ProjectsService {
       name: 'web-design',
       datas: [
         {
-          imgUrl: '../../../assets/web-design/desktop/image-express.jpg',
+          imgUrl: './assets/web-design/desktop/image-express.jpg',
           title: 'EXPRESS',
-          text: 'A multi-carrier shipping website for ecommerce businesses',
+          text: 'A multi-carrier shipping website for ecommerce businesses'
         },
         {
-          imgUrl: '../../../assets/web-design/desktop/image-transfer.jpg',
+          imgUrl: './assets/web-design/desktop/image-transfer.jpg',
           title: 'TRANSFER',
-          text: 'Site for low-cost money transfers and sending money within seconds',
+          text:
+            'Site for low-cost money transfers and sending money within seconds'
         },
         {
-          imgUrl: '../../../assets/web-design/desktop/image-photon.jpg',
+          imgUrl: './assets/web-design/desktop/image-photon.jpg',
           title: 'PHOTON',
-          text: 'A state-of-the-art music player with high-resolution audio and DSP effects',
+          text:
+            'A state-of-the-art music player with high-resolution audio and DSP effects'
         },
         {
-          imgUrl: '../../../assets/web-design/desktop/image-builder.jpg',
+          imgUrl: './assets/web-design/desktop/image-builder.jpg',
           title: 'BUILDER',
-          text: 'Connects users with local contractors based on their location',
+          text: 'Connects users with local contractors based on their location'
         },
         {
-          imgUrl: '../../../assets/web-design/desktop/image-blogr.jpg',
+          imgUrl: './assets/web-design/desktop/image-blogr.jpg',
           title: 'BLOGR',
-          text: 'Blogr is a platform for creating an online blog or publication',
+          text: 'Blogr is a platform for creating an online blog or publication'
         },
         {
-          imgUrl: '../../../assets/web-design/desktop/image-camp.jpg',
+          imgUrl: './assets/web-design/desktop/image-camp.jpg',
           title: 'CAMP',
-          text: 'Get expert training in coding, data, design, and digital marketing',
-        },
-      ],
+          text:
+            'Get expert training in coding, data, design, and digital marketing'
+        }
+      ]
     },
     {
       name: 'app-design',
       datas: [
         {
-          imgUrl: '../../../assets/app-design/desktop/image-airfilter.jpg',
+          imgUrl: './assets/app-design/desktop/image-airfilter.jpg',
           title: 'AIRFILTER',
-          text: 'Solving the problem of poor indoor air quality by filtering the air',
+          text:
+            'Solving the problem of poor indoor air quality by filtering the air'
         },
         {
-          imgUrl: '../../../assets/app-design/desktop/image-eyecam.jpg',
+          imgUrl: './assets/app-design/desktop/image-eyecam.jpg',
           title: 'EYECAM',
-          text: 'Product that lets you edit your favorite photos and videos at any time',
+          text:
+            'Product that lets you edit your favorite photos and videos at any time'
         },
         {
-          imgUrl: '../../../assets/app-design/desktop/image-faceit.jpg',
+          imgUrl: './assets/app-design/desktop/image-faceit.jpg',
           title: 'FACEIT',
-          text: 'Get to meet your favorite internet superstar with the faceit app',
+          text:
+            'Get to meet your favorite internet superstar with the faceit app'
         },
         {
-          imgUrl: '../../../assets/app-design/desktop/image-todo.jpg',
+          imgUrl: './assets/app-design/desktop/image-todo.jpg',
           title: 'TODO',
-          text: 'A todo app that features cloud sync with light and dark mode',
+          text: 'A todo app that features cloud sync with light and dark mode'
         },
         {
-          imgUrl: '../../../assets/app-design/desktop/image-loopstudios.jpg',
+          imgUrl: './assets/app-design/desktop/image-loopstudios.jpg',
           title: 'LOOPSTUDIOS',
-          text: 'A VR experience app made for Loopstudios',
-        },
-      ],
+          text: 'A VR experience app made for Loopstudios'
+        }
+      ]
     },
     {
       name: 'graphic-design',
       datas: [
         {
-          imgUrl: '../../../assets/graphic-design/desktop/image-change.jpg',
+          imgUrl: './assets/graphic-design/desktop/image-change.jpg',
           title: 'TIM BROWN',
-          text: 'A book cover designed for Tim Brown’s new release, ‘Change’',
+          text: 'A book cover designed for Tim Brown’s new release, ‘Change’'
         },
         {
-          imgUrl:
-            '../../../assets/graphic-design/desktop/image-boxed-water.jpg',
+          imgUrl: './assets/graphic-design/desktop/image-boxed-water.jpg',
           title: 'BOXED WETER',
-          text: 'A simple packaging concept made for Boxed Water',
+          text: 'A simple packaging concept made for Boxed Water'
         },
         {
-          imgUrl: '../../../assets/graphic-design/desktop/image-science.jpg',
+          imgUrl: './assets/graphic-design/desktop/image-science.jpg',
           title: 'SCIENCE',
-          text: 'A poster made in collaboration with the Federal Art Project',
-        },
-      ],
-    },
+          text: 'A poster made in collaboration with the Federal Art Project'
+        }
+      ]
+    }
   ];
   constructor() {}
 
